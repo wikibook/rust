aa92eb7a2c1e10fd66d1e5b32be4c1f55a117506
freshness <82712636+highfreshness@users.noreply.github.com>
2024-05-27 17:25:29 +0900

    Update moon.rs
    mismatched types 에러 수정

--- src/ch1/moon.rs
+++ src/ch1/moon.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let moon: f32 = 384400;
-    let car: f32 = 80;
-    let btrain: f32 = 300;
+    let moon: f32 = 384400.0;
+    let car: f32 = 80.0;
+    let btrain: f32 = 300.0;
     println!("달까지 자동차로 {}일", moon / car / 24.0);
     println!("달까지 KTX로 {}일", moon / btrain / 24.0);
 }
